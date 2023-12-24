3a3f75d6fcce3f26af03437bafd5123d5a8b1fad
huynhletanphat <138270919+huynhletanphat@users.noreply.github.com>
2023-12-24 09:02:49 +0000

    update pexels (random link of keyword)

--- media/pexels.js
+++ media/pexels.js
@@ -11,36 +11,28 @@ function getRandomIndex(arr) {
 }
 
 router.get('/', async (req, res) => {
-  const imgParam = req.query.img; // Lấy giá trị của tham số 'img' từ URL
+  const imgParam = req.query.img; 
 
   try {
-    // Sử dụng giá trị 'img' để gửi yêu cầu tới Pexels API
+
     const response = await axios.get(`https://api.pexels.com/v1/search?query=${imgParam}&per_page=3`, {
       headers: {
-        Authorization: 'xX2KgxF3E0UTIeUq7DJf5kUDHRdMRvkwht19ovBFK1rte4LxQGzE4HTP' // Thay thế YOUR_API_KEY bằng API key của bạn từ Pexels
+        Authorization: 'xX2KgxF3E0UTIeUq7DJf5kUDHRdMRvkwht19ovBFK1rte4LxQGzE4HTP' 
       }
     });
 
     const photos = response.data.photos;
 
-    // Lấy danh sách các link ảnh
     const imageLinks = photos.map(photo => photo.src.original);
 
-    // Chọn ngẫu nhiên một liên kết từ danh sách
     const randomIndex = getRandomIndex(imageLinks);
     const randomImage = imageLinks[randomIndex];
 
-    // Tạo một object JSON mới với cấu trúc mong muốn và keyword
-    const result = {
-      keyword: imgParam,
-      img: randomImage // Chỉ trả về một link ảnh ngẫu nhiên
-    };
 
-    // Ghi logs
     const logEntry = {
       Time: moment().tz('Asia/Ho_Chi_Minh').format('YYYY-MM-DD HH:mm:ss'),
-      KEYWORD: result['keyword'],
-      IMAGE: result['img']
+      KEYWORD: imgParam,
+      IMAGE: randomImage 
     };
 
     const logString = JSON.stringify(logEntry, null, 2);
@@ -50,7 +42,11 @@ router.get('/', async (req, res) => {
       }
     });
 
-    res.json(result); // Trả về dữ liệu JSON của một link ảnh ngẫu nhiên từ Pexels API
+    const result = {
+      img: randomImage
+    };
+
+    res.json(result);
   } catch (error) {
     console.error(error);
     res.status(500).send('Error fetching data');
