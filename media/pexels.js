0a385605a277cd8a9f6d5035fa08a3cd8d9749b7
huynhletanphat <138270919+huynhletanphat@users.noreply.github.com>
2023-12-24 08:58:01 +0000

    fix json pexels

--- media/pexels.js
+++ media/pexels.js
@@ -4,6 +4,12 @@ const router = express.Router();
 const fs = require('fs');
 const moment = require('moment-timezone');
 
+function getRandomIndex(arr) {
+  const max = arr.length - 1;
+  const min = 0;
+  return Math.floor(Math.random() * (max - min + 1)) + min;
+}
+
 router.get('/', async (req, res) => {
   const imgParam = req.query.img; // Lấy giá trị của tham số 'img' từ URL
 
@@ -17,17 +23,24 @@ router.get('/', async (req, res) => {
 
     const photos = response.data.photos;
 
+    // Lấy danh sách các link ảnh
+    const imageLinks = photos.map(photo => photo.src.original);
+
+    // Chọn ngẫu nhiên một liên kết từ danh sách
+    const randomIndex = getRandomIndex(imageLinks);
+    const randomImage = imageLinks[randomIndex];
+
     // Tạo một object JSON mới với cấu trúc mong muốn và keyword
     const result = {
       keyword: imgParam,
-      imgs: photos.map(photo => photo.src.original) // Lấy danh sách các link ảnh từ response
+      img: randomImage // Chỉ trả về một link ảnh ngẫu nhiên
     };
 
     // Ghi logs
     const logEntry = {
       Time: moment().tz('Asia/Ho_Chi_Minh').format('YYYY-MM-DD HH:mm:ss'),
       KEYWORD: result['keyword'],
-      IMAGES: result['imgs']
+      IMAGE: result['img']
     };
 
     const logString = JSON.stringify(logEntry, null, 2);
@@ -37,7 +50,7 @@ router.get('/', async (req, res) => {
       }
     });
 
-    res.json(result); // Trả về dữ liệu JSON của các hình ảnh từ Pexels API theo cấu trúc mới
+    res.json(result); // Trả về dữ liệu JSON của một link ảnh ngẫu nhiên từ Pexels API
   } catch (error) {
     console.error(error);
     res.status(500).send('Error fetching data');
